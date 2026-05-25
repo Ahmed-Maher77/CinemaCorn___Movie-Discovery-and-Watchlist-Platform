de5f7a537fa58ce109103f35b2d7a33afbfba6fc
“Ahmed <ahmedmaheraljwhry057@gmail.com>
2026-05-25 12:49:27 +0300

    feat: add HomeResults and HomeSearch styles; refactor MovieCard component structure

--- src/components/MoviesContainer/MovieCard/MovieCard.jsx
+++ src/components/MoviesContainer/MovieCard/MovieCard.jsx
@@ -1,13 +1,39 @@
-import React from 'react'
+import MovieCardFooter from "../MovieCardFooter/MovieCardFooter";
+import MovieCardInfo from "../MovieCardInfo/MovieCardInfo";
+import MovieCardPoster from "../MovieCardPoster/MovieCardPoster";
 
-const MovieCard = ({ title, year, rating }) => {
-  return (
-    <article>
-      <h3>{title}</h3>
-      <p>Year: {year}</p>
-      <p>Rating: {rating}</p>
-    </article>
-  )
-}
+const MovieCard = ({
+    id,
+    title,
+    release_date,
+    vote_average,
+    poster_path,
+    overview,
+}) => {
+    const posterUrl = poster_path
+        ? `https://image.tmdb.org/t/p/w500${poster_path}`
+        : null;
 
-export default MovieCard
+    const releaseYear = release_date ? release_date.slice(0, 4) : "N/A";
+
+    return (
+        <article className="movieCard">
+          {/* ======= poster ======= */}
+            <MovieCardPoster posterUrl={posterUrl} title={title} id={id} />
+
+            {/* ======= info & rating ======= */}
+            <div className="movieCard-content">
+                <MovieCardInfo
+                    title={title}
+                    releaseYear={releaseYear}
+                    overview={overview}
+                />
+
+                {/* ======= footer ======= */}
+                <MovieCardFooter rating={vote_average} id={id} />
+            </div>
+        </article>
+    );
+};
+
+export default MovieCard;
