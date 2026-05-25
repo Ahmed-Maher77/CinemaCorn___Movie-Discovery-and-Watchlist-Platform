9a2d845d04823d2ddecc5fc901fa53a87491af03
“Ahmed <ahmedmaheraljwhry057@gmail.com>
2026-05-25 20:56:32 +0300

    feat: add prop-types dependency; update ErrorMessage component to enforce prop validation

--- src/components/ErrorMessage/ErrorMessage.jsx
+++ src/components/ErrorMessage/ErrorMessage.jsx
@@ -1,13 +1,18 @@
+import PropTypes from "prop-types";
 import errorIcon from "../../assets/images/error.svg";
 import "./ErrorMessage.css";
 
 const ErrorMessage = ({ message }) => {
-  return (
-    <div className="error-message">
-        <img src={errorIcon} alt="Error" />
-      <p>{message}</p>
-    </div>
-  )
-}
+    return (
+        <div className="error-message">
+            <img src={errorIcon} alt="Error" />
+            <p>{message}</p>
+        </div>
+    );
+};
 
-export default ErrorMessage
+ErrorMessage.propTypes = {
+    message: PropTypes.string.isRequired,
+};
+
+export default ErrorMessage;
